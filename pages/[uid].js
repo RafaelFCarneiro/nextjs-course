ad0c39ba9d2025462d157c87ea981c73dc1a73af
rafael.carneiro <Rafael_Carneiro@gap.com>
2021-11-24 10:49:43 -0300

    dummy sample server side pre-rendering

---
+++ pages/[uid].js
@@ -0,0 +1,16 @@
+function UserIdPage(props) {
+  return <h1>{props.id}</h1>;
+}
+
+export default UserIdPage;
+
+export const getServerSideProps = async (ctx) => {
+  const { params } = ctx;
+  const userId = params.uid;
+
+  return {
+    props: {
+      id: `user-id-${userId}`,
+    },
+  };
+};
---
+++ pages/user-profile.js
@@ -0,0 +1,18 @@
+function UserProfilePage(props) {
+  return <h1>{props.username}</h1>;
+}
+
+export default UserProfilePage;
+
+export const getServerSideProps = async (ctx) => {
+  const { params, req, res } = ctx;
+
+  console.log(req);
+  console.log(res);
+  
+  return {
+    props: {
+      username: "Rafael",
+    },
+  };
+};
