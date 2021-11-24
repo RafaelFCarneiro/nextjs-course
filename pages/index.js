988df409e6969714d752fac488eaee916f45f07d
rafael.carneiro <Rafael_Carneiro@gap.com>
2021-11-24 13:57:40 -0300

    cleaning for the next module

--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,9 @@
-import path from "path";
-import fs from "fs/promises";
-
-import Link from "next/link";
-
-function HomePage(props) {
-  const { products } = props;
-
+function HomePage() {
   return (
-    <ul>
-      {products.map((product) => (
-        <li key={product.id}>
-          <Link href={`/products/${product.id}`}>{product.title}</Link>
-        </li>
-      ))}
-    </ul>
+    <div>
+      <h1> Home Page </h1>
+    </div>
   );
-}
-
-export async function getStaticProps(context) {
-  console.log("(Re-)Generating index...");
-  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
-  const data = JSON.parse(await fs.readFile(filePath));
-
-  if (!data) {
-    return {
-      redirect: {
-        destination: "/no-data",
-      },
-    };
-  }
-
-  const { products } = data;
-  if (!products.length) {
-    return { notFound: true };
-  }
-
-  return {
-    props: data,
-    revalidate: 10,
-  };
 }
 
 export default HomePage;
