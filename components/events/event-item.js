6bd249cf9e484a8b5b73c360c5d75713a9bdb264
rafael.carneiro <Rafael_Carneiro@gap.com>
2021-12-09 23:47:43 -0300

    Optimizing NextJs Apps: images

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,24 +1,25 @@
 /* eslint-disable @next/next/no-img-element */
-import Button from '../ui/button';
-import DateIcon from '../icons/date-icon';
-import AddressIcon from '../icons/address-icon';
-import ArrowRightIcon from '../icons/arrow-right-icon';
-import classes from './event-item.module.css';
+import Image from "next/image";
+import Button from "../ui/button";
+import DateIcon from "../icons/date-icon";
+import AddressIcon from "../icons/address-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon";
+import classes from "./event-item.module.css";
 
 function EventItem(props) {
   const { id, title, image, date, location } = props;
 
-  const formattedDate = new Date(date).toLocaleDateString('en-US', {
-    day: 'numeric',
-    month: 'long',
-    year: 'numeric',
+  const formattedDate = new Date(date).toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
   });
-  const formattedAddress = location.replace(', ', '\n');
+  const formattedAddress = location.replace(", ", "\n");
   const exploreLink = `/events/${id}`;
 
   return (
     <li className={classes.item}>
-      <img src={`/${image}`} alt={title} />
+      <Image src={`/${image}`} alt={title} width={250} height={160} />
       <div className={classes.content}>
         <div className={classes.summary}>
           <h2>{title}</h2>
